dfa59566872dad16d2210672ac61cf85f912463e
Francisco Medina <josef.medina@vass.com.mx>
2022-12-30 19:42:33 -0600

    login and register mockups

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function Login() {
+  const [registerValues, setRegisterValues] = useState({
+    email:"",
+    password:""
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(registerValues)
+  }
+
   return (
-    <div>Login</div>
-  )
+    <div className='container'>
+      <h2>Login</h2>
+      <form onSubmit={(e)=>handleSubmit(e)}>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input 
+            type='email' 
+            name='email' 
+            placeholder='Email'
+            onChange={(e) => 
+              setRegisterValues({...registerValues, [e.target.name]:e.target.value})
+            } 
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input 
+            type='password' 
+            name='password' 
+            placeholder='Password'
+            onChange={(e) => 
+              setRegisterValues({...registerValues, [e.target.name]:e.target.value})
+            } 
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          No account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+      <ToastContainer/>
+    </div>
+  );
 }
--- src/views/Register/index.js
+++ src/views/Register/index.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function Register() {
+  const [registerValues, setRegisterValues] = useState({
+    email:"",
+    password:""
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(registerValues)
+  }
+
   return (
-    <div>Register</div>
-  )
+    <div className='container'>
+      <h2>Register</h2>
+      <form onSubmit={(e)=>handleSubmit(e)}>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input 
+            type='email' 
+            name='email' 
+            placeholder='Email'
+            onChange={(e) => 
+              setRegisterValues({...registerValues, [e.target.name]:e.target.value})
+            } 
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input 
+            type='password' 
+            name='password' 
+            placeholder='Password'
+            onChange={(e) => 
+              setRegisterValues({...registerValues, [e.target.name]:e.target.value})
+            } 
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          Already have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+      <ToastContainer/>
+    </div>
+  );
 }
