60921d9beedb814487e285b7b58b52b2bc1dc006
Francisco Medina <josef.medina@vass.com.mx>
2023-01-02 20:21:15 -0600

    check user auth on back

--- src/views/Admin/index.js
+++ src/views/Admin/index.js
@@ -1,17 +1,46 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom'
+import { toast, ToastContainer } from 'react-toastify';
 
 export default function Admin() {
   const navigate = useNavigate();
+  const [cookies, setCookie, removeCookie] = useCookies([]);
+
+  useEffect(()=>{
+    const verifyUser = async () => {
+      if(!cookies.jwt){
+        navigate("/login");
+      } else {
+        const { data } = await axios.post(
+          "http://localhost:5001/auth/checkUser",
+          {},
+          {withCredentials:true}
+        );
+        if(!data.status){
+          removeCookie("jwt");
+          navigate("/login");
+        } else{
+          toast(`Hello ${data.user}`, {theme:"dark"})
+        }
+      }
+    };
+    verifyUser();
+  }, [cookies, navigate, removeCookie])
 
   const logOut = () => {
+    removeCookie("jwt")
     navigate("/login");
   };
 
   return (
-    <div>
-      <h2>Admin</h2>
-      <button onClick={logOut}> Logout </button>
-    </div>
+    <>
+      <div>
+        <h2>Admin</h2>
+        <button onClick={logOut}> Logout </button>
+      </div>
+      <ToastContainer/>
+    </>
   )
 }
